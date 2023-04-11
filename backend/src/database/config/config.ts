8ce18c2cc2c5b915bb9780740638046b108d04d2
Igor Brizack <igor_brizack@hotmail.com>
2023-04-11 13:25:25 -0300

    feat: backend server

--- backend/src/database/config/config.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-
-const mongoose = require('mongoose')
-
-const LOGIN = process.env.MONGO_USERNAME
-const PASSWORD = process.env.MONGO_PASSWORD
-
-const MONGODB_DSN = `mongodb://${LOGIN}:${PASSWORD}@localhost:27017`
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,15 @@
-import app from "./App"
+import 'dotenv/config';
+import app from './App';
+import connectToDatabase from './Models/Connection';
 
-const PORT = process.env.PORT ?? 3001
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
+const PORT = process.env.PORT || 3001;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(0);
+  });
