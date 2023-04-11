a0b9ff2b4ff4dfc6b6f3ccbefc1a75de082badd4
Igor Brizack <igor_brizack@hotmail.com>
2023-04-11 13:08:36 -0300

    fix: docker-compose

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -1,1 +1,8 @@
-db = connect("localhost:27017")
+import 'dotenv/config'
+
+const mongoose = require('mongoose')
+
+const LOGIN = process.env.MONGO_USERNAME
+const PASSWORD = process.env.MONGO_PASSWORD
+
+const MONGODB_DSN = `mongodb://${LOGIN}:${PASSWORD}@localhost:27017`
